d434c63cd5660d636000ae086695e9f25d190e3c
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-09-18 10:28:07 -0400

    added the view-model code

--- app/src/main/java/ca/tetervak/dicerollerlab/MainViewModel.kt
+++ app/src/main/java/ca/tetervak/dicerollerlab/MainViewModel.kt
@@ -1,8 +1,20 @@
 package ca.tetervak.dicerollerlab
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
 
+    private val _rollResult = MutableLiveData<Int>()
+    val rollResult: LiveData<Int> = _rollResult
 
+    fun roll() {
+        _rollResult.value = getRollResult()
+    }
+
+    private fun getRollResult(): Int {
+        val dice = Dice(6)
+        return dice.roll()
+    }
 }
